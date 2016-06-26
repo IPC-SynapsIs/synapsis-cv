59c481dc4967a9d94453d010ba960347981e5313
mbasri <basrimohamed@live.fr>
2016-06-26 11:49:46 +0200

    [CV][DOCKER]Add dockerfile

--- src/main/java/org/synapsis/resource/impl/CurriculumVitaeResource.java
+++ src/main/java/org/synapsis/resource/impl/CurriculumVitaeResource.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.synapsis.Repository.ICurriculumVitaeRepository;
+import org.synapsis.repository.ICurriculumVitaeRepository;
 import org.synapsis.entity.CurriculumVitae;
 import org.synapsis.resource.ICurriculumVitaeResource;
 
