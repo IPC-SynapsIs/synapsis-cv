6cc22d9aec144b53b2d21af19b4699f5445d88b9
mbasri <basrimohamed@live.fr>
2016-10-03 22:30:25 +0200

    [CV] Delete Eureka

--- src/main/java/org/ipc/synapsis/curriculumvitae/CurriculumVitaeServerClass.java
+++ src/main/java/org/ipc/synapsis/curriculumvitae/CurriculumVitaeServerClass.java
@@ -2,12 +2,10 @@ package org.ipc.synapsis.curriculumvitae;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableJpaRepositories(basePackages = {"org.ipc.synapsis.curriculumvitae.repository"})
 public class CurriculumVitaeServerClass {
 
